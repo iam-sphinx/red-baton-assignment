197a49eca7282e73505d3a482f5fade432fa898e
iam-sphinx <kandarp1803@gmail.com>
2024-01-31 15:30:26 +0530

    Done

--- server/src/app.js
+++ server/src/app.js
@@ -8,12 +8,7 @@ import axios from "axios";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
